2fe8d04f48bbb5ac0f1e9ad026a79a235e1f2fb1
MatanDaniel <93178535+MatanDaniel@users.noreply.github.com>
2025-05-21 17:42:53 +0300

    Added login button + viewBinding for login activity.

--- app/src/main/java/com/example/chatapplication/LoginActivity.java
+++ app/src/main/java/com/example/chatapplication/LoginActivity.java
@@ -8,17 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.chatapplication.databinding.ActivityLoginBinding;
+
 public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding loginBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(loginBinding.getRoot());
     }
 }
